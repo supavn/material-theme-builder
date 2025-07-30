7ab763f652047b15dc0a99ecb70354948afb8171
Thanh Tùng <thanhtung.uet@gmail.com>
2025-07-30 11:12:22 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
-  base: process.env.NODE_ENV === "production" ? "/material-theme-builder/" : "/",
+  base: "/",
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
